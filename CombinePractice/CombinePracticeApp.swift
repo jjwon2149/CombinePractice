02660cefa53b7a589c5f885739612ff9626f9228
Smith <jjwon2149@gmail.com>
2024-06-21 01:10:01 +0900

    Initialize CombinePractice Project

---
+++ CombinePractice/CombinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePracticeApp.swift
+//  CombinePractice
+//
+//  Created by 정종원 on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CombinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
