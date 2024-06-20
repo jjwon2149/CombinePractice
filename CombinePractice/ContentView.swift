29e7152b63e0b714dfb242c267569cecfad2f647
Smith <jjwon2149@gmail.com>
2024-06-21 01:25:58 +0900

    feat: map, assign, store을 사용하여 텍스트 변경 컴바인

--- CombinePractice/ContentView.swift
+++ CombinePractice/ContentView.swift
@@ -6,14 +6,39 @@
 //
 
 import SwiftUI
+import Combine
+
+class ViewModel: ObservableObject {
+    @Published var inputText: String = ""
+    @Published var isButtonEnabled: Bool = false
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        $inputText
+            .map { $0.count > 5 }
+            .assign(to: \.isButtonEnabled, on: self)
+            .store(in: &cancellables)
+    }
+}
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("input Text", text: $viewModel.inputText)
+                .border(.black)
+                .padding()
+            
+            Button {
+               print("Button Clicked")
+            } label: {
+                Text("Submit")
+            }
+            .disabled(!viewModel.isButtonEnabled)
+            .padding()
         }
         .padding()
     }
